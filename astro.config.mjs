9e04026ecdb1757d0e8950664fbcc8897159ae99
Andrew Cauchi <drewcauchi@gmail.com>
2025-02-09 23:36:14 +0100

    Added node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
+import node from '@astrojs/node';
 
+// To come from Environment variable
 const testClient = "stradivarius"
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [vue()],
+    output: 'server',
+    adapter: node({
+        mode: 'standalone',
+    }),
     devToolbar: {
         enabled: false,
     },
     trailingSlash: 'never',
-    srcDir: `src/${testClient}`
+    srcDir: `src/${testClient}`,
+    outDir: `src/${testClient}/dist`
 });
